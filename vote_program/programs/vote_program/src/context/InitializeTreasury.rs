3edd7d45982459dad016f5961b0a82adb0b9c6fc
0xSunill <sunday7637@gmail.com>
2026-01-19 14:58:51 +0000

    add InitializeTreasury

--- vote_program/programs/vote_program/src/context/InitializeTreasury.rs
+++ vote_program/programs/vote_program/src/context/InitializeTreasury.rs
@@ -1,5 +1,6 @@
 use crate::state::*;
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 #[derive(Accounts)]
 pub struct InitializeTreasury<'info> {
@@ -8,12 +9,12 @@ pub struct InitializeTreasury<'info> {
     #[account(init, payer = authority, space = 8 + TreasuryConfig::INIT_SPACE, seeds = [b"treasury_config"], bump)]
     pub treasury_config_account: Account<'info, TreasuryConfig>,
 
-    #[account(init, payer = authority, space = 8 + Mint::INIT_SPACE, seeds = [b"x_mint"], bump)]
+    #[account(init, payer = authority,mint::authority = mint_authority , mint::decimals = 6, seeds = [b"x_mint"], bump)]
     pub x_mint: Account<'info, Mint>,
 
     pub treasury_token_account: Account<'info, TokenAccount>,
 
-    #[account(mut,seeds = [b"sol_vault"], bump)]  
+    #[account(mut,seeds = [b"sol_vault"], bump)]
     pub sol_vault: Account<'info, TokenAccount>,
 
     #[account(seeds = [b"mint_authority"], bump)]
